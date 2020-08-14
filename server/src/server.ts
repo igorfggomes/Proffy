59de7253a8fa2f7c73e2f0af4f04f27e9d5be580
igorfggomes <igor.fggomes@hotmail.com>
2020-08-14 01:07:56 -0300

    Criar rota

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    const users = [
+        { name: 'Diego', age: 25 },
+        { name: 'Vini', age: 21 },
+    ];
+    
+    return response.json(users);
+});
+
+//localhost:3333/users
+
+app.listen(3333);
